a61990abf33f4da1b81f3054eddbf43b86cac4d8
Yassine Asghar <86357052+yassineasghar@users.noreply.github.com>
2023-03-25 01:58:01 +0100

    Update build.py

--- build.py
+++ build.py
@@ -4,27 +4,29 @@ import requests
 class JenkinsAPI:
     def __init__(self):
         configfile = r'config.yaml'
-        with open(configfile) as file:
+        with open(configfile) as c:
             # config = yaml.safe_load(c)
             config = yaml.load(c, Loader=yaml.FullLoader)
 
-        jenkins_config = config['jenkins']
-        self.username = jenkins_config['user']
-        self.api_token = jenkins_config['apitoken']
-        self.jenkins_url = jenkins_config['server']
-        self.job_name = jenkins_config['job']
-        self.build_token = jenkins_config['buildtoken']
+        jenkinsconfigs = config['jenkins']
+        self.username = jenkinsconfigs['user']
+        self.api_token = jenkinsconfigs['apitoken']
+        self.jenkins_url = jenkinsconfigs['server']
+        self.job_name = jenkinsconfigs['job']
+        self.build_token = jenkinsconfigs['buildtoken']
 
     def build_job(self):
         url = f"{self.jenkins_url}/job/{self.job_name}/build"
         auth = (self.username, self.api_token)
         params = {'token': self.build_token}
-        response = requests.post(url, auth=auth, params=params)
-        if response.status_code == 201:
+
+        resp = requests.post(url, auth=auth, params=params)
+
+        if resp.status_code == 201:
             print(f"[INFO] : Successfully triggered {self.job_name}")
         else:
             print(f"[ERROR] : Failed to trigger build for job {self.job_name}")
-            print(f"[FAIL] Response status code: {response.status_code}")
+            print(f"[FAIL] resp status code: {resp.status_code}")
 
 if __name__ == '__main__':
     api = JenkinsAPI()
