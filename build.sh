c78fb0523d9096fd84a4e9e76735d72d363ad93c
Yassine Asghar <86357052+yassineasghar@users.noreply.github.com>
2023-03-25 02:47:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # It uses the credentials of user USER with API token USER_API_TOKEN and includes 
 # a build token BUILD_TOKEN_NAME as a parameter in the URL.
 # This curl command perform the same task as build.py
+# FOLDER_NAME , when the job is located on a folder , if not use the following : 
+# curl -X POST -u "USER:USER_API_TOKEN" "JENKINS_URL/job/JOB_NAME/build?token=BUILD_TOKEN_NAME"
 
 
 
-curl -X POST -u "USER:USER_API_TOKEN" "JENKINS_URL/job/JOB_NAME/build?token=BUILD_TOKEN_NAME"
+curl -X POST -u "USER:USER_API_TOKEN" "JENKINS_URL/job/FOLDER_NAME/job/JOB_NAME/build?token=BUILD_TOKEN_NAME"
